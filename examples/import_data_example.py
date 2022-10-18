da3a372e227cc61940bc6520bda5a069ef565aab
Knut Andreas Kvåle <knut.a.kvale@gmail.com>
2022-10-18 13:41:58 +0200

    Update import_data_example.py

--- examples/import_data_example.py
+++ examples/import_data_example.py
@@ -16,29 +16,31 @@ with h5py.File(fname, 'r') as hf:
     
 #%% Plot sensors
 import plotly.io as pio
-pio.renderers.default='browser'
+pio.renderers.default = 'browser'
 
 with h5py.File(fname, 'r') as hf:
     rec_names = list(hf.keys())
     hf_rec = hf[rec_names[-1]]
     fig = plot_sensors(hf_rec, view_axis=2)
     fig.show()
-    
-#%% Some components only
-component_dict = {'wave_radars': ['h'], 
-                  'accelerometers': ['x', 'y', 'z'],
-                  'anemometers': ['U']}
 
+#%% Full h5 datastructure from chosen file
+rec_name = 'NTNU142M-2016-12-26_22-33-33'
+hf = h5py.File(fname, 'r')[rec_name]
+
+# E.g. you could grab transformation matrix from GNSS sensor (North, East compared to X, Y)
+tmat = hf['displacement']['GNSS'].attrs['transformation_matrix']
+
+#%% Some components only
+component_dict = {'wave': ['h'], 
+                  'acceleration': ['x', 'y', 'z'],
+                  'wind': ['U']}
 
 with h5py.File(fname, 'r') as hf:
     rec_names = list(hf.keys())
     hf_rec = hf[rec_names[-1]]
-    
-    # Single recording
-    data_single = export_from_hdf(hf[rec_names[0]], component_dict=component_dict)
-    data_single = data_single.set_index('t')
-    
-    # Multiple recordings(example shows last 10 recs)
+
+    # Select last 10 recs
     data_multiple = export_from_multirec_hdf(hf, rec_names[-10:], component_dict=component_dict, decimation_factor=100)
 
 data_multiple.plot()
