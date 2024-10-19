54415ba6c7a98b4cbc47a55b166d53a65dc9473c
Knut Andreas Kvåle <knutankv@users.noreply.github.com>
2024-10-19 15:54:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opyndata",
-    version="0.0.1",
+    version="0.0.2",
     author="",
     author_email="",
     description="OpyNDATA toolbox for import and postprocessing of measurement data",
